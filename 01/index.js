80c04a603b484d582a3c05932475cf78adf81be6
ingrid <msagrerom@inee.edu.mx>
2019-11-29 15:15:58 -0600

    refactor code 01

--- 01/index.js
+++ 01/index.js
@@ -1,25 +1,17 @@
-function playSound(e){
-    //console.log(e);
-    //console.log(e.keyCode)
+const playSound = (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    //console.log(audio);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if(!audio) return; //stop function
+    if(!audio) return; 
     key.classList.add('playing');
-    audio.currentTime = 0; //rewind to the start
+    audio.currentTime = 0;
     audio.play()
-    //console.log(key);
-    
 }
 
-function removeTransition(e) {
-    //console.log(e);
-    //console.log(e.type);
-    //console.log(e.propertyName);
+const removeTransition = (e) => {
     if (e.propertyName !== 'transform') return;
-   this.classList.remove('playing');
+    e.target.classList.remove('playing');
 }
+
 const keys= document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
 window.addEventListener('keydown', playSound);
